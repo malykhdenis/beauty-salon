50ad11df3ea2bdada5ffc70c33ef470eb1ebc430
Your Name <you@example.com>
2024-07-24 10:55:38 +0500

    add login/logout

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views as acc
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('login', acc.LoginView.as_view(), name='login'),
+    path('logout', acc.LogoutView.as_view(), name='logout'),
 ]
