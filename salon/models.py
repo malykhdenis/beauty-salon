a68223ffe0dc7db035b2768fc7829330f9f6a3b5
Sterrrrest <sterrrrest@gmail.com>
2024-07-25 16:24:46 +0300

    Completed models

--- salon/models.py
+++ salon/models.py
@@ -1,42 +1,93 @@
 from django.db import models
-from django.utils import timezone
+from django.core.validators import MinValueValidator
+
+# from django.utils import timezone
 from django.contrib.auth.models import User
 # from phonenumber_field.modelfields import PhoneNumberField
 
-
 class Salon(models.Model):
-    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    address = models.TextField('Адрес')
+    name = models.CharField(verbose_name='Название салона', max_length=200, db_index=True)
+    address = models.TextField(verbose_name='Адрес', max_length=200, db_index=True, blank=True)
+
+    class Meta:
+        verbose_name = 'салон'
+        verbose_name_plural = 'салоны'
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField('Название процедуры', max_length=200, db_index=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2,
+                                 validators=[MinValueValidator(0)])
+    salon_service = models.ManyToManyField('Salon', verbose_name='Услуги салона',
+                                           related_name='salon_services', blank=True)
+    # master_service = models.ManyToManyField('Master', verbose_name='Услуги салона',
+    #                                         related_name='master_services', blank=True)
+
+    class Meta:
+        verbose_name = 'услуга'
+        verbose_name_plural = 'услуги'
+
+    def __str__(self):
+        return self.name
 
 
 class Master(models.Model):
     firstname = models.CharField('Имя', max_length=200, db_index=True)
     lastname = models.CharField('Фамилия', max_length=200, db_index=True)
-    speciality = models.CharField('Специальность', max_length=200, db_index=True)
-    foto = models.ImageField(blank=True)
-    salon = models.ManyToManyField('Salon', related_name='masters', blank=True)
+    foto = models.ImageField(verbose_name='Фото', upload_to='foto', blank=True)
+    master_service = models.ManyToManyField('Service', verbose_name='Услуги', related_name='master_services', blank=True)
+    master_salon = models.ManyToManyField('Salon', verbose_name='Салон', related_name='master_salons', blank=True)
+
+    class Meta:
+        verbose_name = 'мастер'
+        verbose_name_plural = 'мастера'
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
 
 
-# class Procedure(models.Model):
-#     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-#     price = models.DecimalField()
-#     master = models.ManyToManyField('Master', related_name='procedures')
+class Time(models.Model):
+    time = models.DateTimeField(verbose_name='Время')
+    available = models.BooleanField(verbose_name='Доступно', default=True)
+    master = models.ManyToManyField('Master', verbose_name='Мастер', related_name='available_time', blank=True)
+    salon = models.ManyToManyField('Salon', verbose_name='Салон', related_name='available_time', blank=True)
 
+    class Meta:
+        verbose_name = 'время'
+        verbose_name_plural = 'время'
 
-# class Feedback(models.Model):
-#     user = models.ForeignKey(User, verbose_name='Пользователь', related_name='complaints', on_delete=models.CASCADE)
-#     master = models.ForeignKey('Master', related_name='feedbacks' ,on_delete=models.CASCADE)
-#     feedback = models.TextField('Feedback')
+    def __str__(self):
+        return str(f'{self.time.hour} : {self.time.minute}{self.time.minute}')
 
+class Feedback(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='complaints', on_delete=models.CASCADE)
+    master = models.ForeignKey('Master', related_name='feedbacks', on_delete=models.CASCADE)
+    feedback = models.TextField(verbose_name='Feedback')
 
-# class Order(models.Model):
-#     user = models.ForeignKey(User, verbose_name='Пользователь', related_name='complaints', on_delete=models.CASCADE)
-#     salon = models.ForeignKey('Salon', related_name='orders' ,on_delete=models.CASCADE)
-#     master = models.ForeignKey('Master', related_name='orders' ,on_delete=models.CASCADE)
-#     procedure = models.ForeignKey('Procedure', related_name='orders' ,on_delete=models.CASCADE)
-#     paid = models.BooleanField()
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
 
+    def __str__(self):
+        return str(self.user)
 
-# class OrderElements(models.Model):
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='orders', on_delete=models.CASCADE)
+    salon = models.ForeignKey(Salon, verbose_name='Салон', related_name='orders', on_delete=models.CASCADE, blank=True)
+    master = models.ForeignKey(Master, verbose_name='Мастер', related_name='orders', on_delete=models.CASCADE, blank=True)
+    service = models.ForeignKey(Service, verbose_name='Услуга', related_name='orders', on_delete=models.CASCADE, blank=True)
+    time = models.ForeignKey(Time, verbose_name='Время', related_name='orders', on_delete=models.CASCADE, blank=True)
+    paid = models.BooleanField(verbose_name='Оплачено', default=False)
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
+
+    def __str__(self):
+        return str(self.user)
+
 
 
