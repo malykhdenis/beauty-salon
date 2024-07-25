c6e6f354446ecef09beeb3c3fcaf08c0518c65c9
Denis Malykh <malykhdenis@gmail.com>
2024-07-25 21:33:58 +0300

    adv link clicks counter

--- salon/admin.py
+++ salon/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Salon, Master, Service, Time, Feedback, Order
+import requests
+from environs import Env
+
+from .models import Advertising, Salon, Master, Service, Time, Feedback, Order
+
+env = Env()
+env.read_env()
 
 
 class MasterinLine(admin.TabularInline):
@@ -53,3 +59,25 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('user', 'master', 'service', 'paid')
     list_filter = ('paid', 'salon', 'master', 'time', 'service',)
     raw_id_fields = ('user', 'salon', 'master', 'time', 'service',)
+
+
+@admin.register(Advertising)
+class AdvertisingModel(admin.ModelAdmin):
+    list_display = ('url', 'text', 'responses',)
+    readonly_fields = ('url', 'responses',)
+
+    def changelist_view(self, request, extra_context=None):
+        advertising = Advertising.objects.all()
+        headers = {
+            "Authorization": f"Bearer {env.str('TLY_API_TOKEN')}"
+        }
+        url = "https://t.ly/api/v1/link/stats"
+        for ad in advertising:
+            params = {"short_url": ad.url}
+            response = requests.get(url,
+                                    headers=headers,
+                                    params=params)
+            response.raise_for_status()
+            ad.responses = response.json()["clicks"]
+        Advertising.objects.bulk_update(advertising, ['responses'])
+        return super().changelist_view(request, extra_context=extra_context)
--- src/settings.py
+++ src/settings.py
@@ -103,3 +103,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
+
+SILENCED_SYSTEM_CHECKS = ["auth.W004"]
