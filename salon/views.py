75595fdfd2000fccb6faee1a0ab8fa073f90ba28
Your Name <you@example.com>
2024-07-30 12:12:42 +0500

    add login_required

--- salon/views.py
+++ salon/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, "salon/index.html")
 
 
+@login_required
 def contacts(request):
     return render(request, "salon/contacts.html")
 
 
+@login_required
 def reviews(request):
     return render(request, "salon/reviews.html")
 
 
+@login_required
 def masters(request):
     return render(request, "salon/master.html")
 
 
+@login_required
 def services(request):
     return render(request, "salon/service.html")
 
@@ -25,5 +30,6 @@ def popup(request):
     return render(request, "salon/popup.html")
 
 
+@login_required
 def service(request):
     return render(request, "salon/service.html")
